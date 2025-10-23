f87552d0e2dac4d760e89e9abe38ba4dc9f10eb7
Mohd Kashan <kashan0716@gmail.com>
2025-10-23 17:43:27 +0530

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -32,18 +32,17 @@ export default function Dashboard() {
     <>
       <Navbar user={user} onLogout={handleLogout} />
 
-      <div className="max-w-6xl mx-auto mt-10 px-4 flex flex-col-reverse lg:flex-col gap-6">
-        {/* Expense Form at bottom */}
-        <div className="bg-gray-50 p-6 rounded-2xl shadow-xl order-2 lg:order-2">
-          <ExpenseForm refresh={getExpenses} />
+      <div className="max-w-6xl mx-auto mt-10 px-4 flex flex-col gap-6">
+        {/* Expense List at top */}
+        <div className="bg-white p-6 rounded-2xl shadow-xl">
+          <ExpenseList data={expenses} refresh={getExpenses} />
         </div>
 
-        {/* Expense List at top */}
-        <div className="bg-white p-6 rounded-2xl shadow-xl order-1 lg:order-1">
-          <ExpenseList data={expenses} refresh={getExpenses} />
+        {/* Expense Form at bottom */}
+        <div className="bg-gray-50 p-6 rounded-2xl shadow-xl">
+          <ExpenseForm refresh={getExpenses} />
         </div>
       </div>
     </>
   );
 }
-
