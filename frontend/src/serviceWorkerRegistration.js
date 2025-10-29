574c589fde1afbc6ed05c7aee9112307b06b639b
Mohd Kashan <kashan0716@gmail.com>
2025-10-29 20:21:59 +0530

    dd

---
+++ frontend/src/serviceWorkerRegistration.js
@@ -0,0 +1,11 @@
+// In src/serviceWorkerRegistration.js
+export function register() {
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("/service-worker.js")
+        .then((reg) => console.log("Service Worker registered:", reg))
+        .catch((err) => console.error("Service Worker registration failed:", err));
+    });
+  }
+}
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Expense Tracker App",
+        short_name: "ExpenseTracker",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#111827",
+        icons: [
+          { src: "/icons/icon.png", sizes: "192x192", type: "image/png" },
+          { src: "/icons/icon2.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+    }),
+  ],
 });
